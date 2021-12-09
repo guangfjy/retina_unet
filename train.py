63bda604f3de76f9d8fb1c2113aa14475d270358
guangfjy <guangfjy@gmail.com>
2021-12-09 18:45:47 +0800

    fit to debug on local

--- train.py
+++ train.py
@@ -10,8 +10,7 @@ from core.unet import Unet
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('logdir', None, 'WithTensorBoard logdir.')
-flags.DEFINE_string('label_file_path', None,
-                    'Path of training dataset label file.')
+flags.DEFINE_string('label_file_path', None, 'Path of training dataset label file.')
 flags.DEFINE_integer('seed', 0, 'Seed to training dataset.')
 flags.DEFINE_float('initial_lr', 3e-4, 'Initial learning rate.')
 flags.DEFINE_integer('batch_size', 4, 'Global batch size.')
@@ -21,121 +20,129 @@ flags.DEFINE_boolean('preprocess', False, 'Whether to use preprocessed image.')
 flags.DEFINE_boolean('augmentation', False, 'Whether to use augmentation.')
 flags.DEFINE_integer('image_size', 1024, 'Size of each image side to use.')
 flags.DEFINE_integer('num_classes', 2, 'Number of classes.')
-flags.DEFINE_boolean('transpose_conv', False,
-                     'Whether to use Conv2DTranspose as upsample layer.')
+flags.DEFINE_boolean('transpose_conv', False, 'Whether to use Conv2DTranspose as upsample layer.')
 
 
 def _learning_rate_schedule(global_step_value, max_iters, initial_lr):
-  """Calculates learning_rate with linear decay.
+    """Calculates learning_rate with linear decay.
 
-  Args:
-    global_step_value: int, global step.
-    max_iters: int, maximum iterations.
-    initial_lr: float, initial learning rate.
+    Args:
+        global_step_value: int, global step.
+        max_iters: int, maximum iterations.
+        initial_lr: float, initial learning rate.
 
-  Returns:
-    lr: float, learning rate.
-  """
-  lr = initial_lr * (1.0 - global_step_value / max_iters)
-  return lr
+    Returns:
+        lr: float, learning rate.
+    """
+    lr = initial_lr * (1.0 - global_step_value / max_iters)
+    return lr
 
 
 def main(argv):
+    FLAGS.logdir = "log/retina"
+    FLAGS.label_file_path = "data/CHASEDB/training.txt"
+    FLAGS.batch_size = 4
+    FLAGS.max_iters = 2000
+    FLAGS.preprocess = True
+    FLAGS.transpose_conv = True
 
-  max_iters = FLAGS.max_iters
-  initial_lr = FLAGS.initial_lr
+    max_iters = FLAGS.max_iters
+    initial_lr = FLAGS.initial_lr
 
-  # -------------------------------------------------
-  # Create the training set.
-  train_dataset = CreateDataset(
-      file_path=FLAGS.label_file_path,
-      image_size=FLAGS.image_size,
-      batch_size=FLAGS.batch_size,
-      preprocess=FLAGS.preprocess,
-      augmentation=FLAGS.augmentation,
-      seed=FLAGS.seed)
-  train_iter = iter(train_dataset)
+    # -------------------------------------------------
+    # Create the training set.
+    train_dataset = CreateDataset(
+        file_path=FLAGS.label_file_path,
+        image_size=FLAGS.image_size,
+        batch_size=FLAGS.batch_size,
+        preprocess=FLAGS.preprocess,
+        augmentation=FLAGS.augmentation,
+        repeat=False,
+        seed=FLAGS.seed)
+    train_iter = iter(train_dataset)
 
-  model = Unet(
-      data_format='channels_last',
-      classes=FLAGS.num_classes,
-      transpose_conv=FLAGS.transpose_conv)
-  
-  loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-  optimizer = tf.keras.optimizers.Adam(learning_rate=initial_lr)
+    model = Unet(
+        data_format='channels_last',
+        classes=FLAGS.num_classes,
+        transpose_conv=FLAGS.transpose_conv)
 
-  # Setup summary writer.
-  summary_writer = tf.summary.create_file_writer(
-    os.path.join(FLAGS.logdir, 'train_logs'), flush_millis=10000)
+    loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=initial_lr)
 
-  # Create a checkpoint directory to store the checkpoints.
-  checkpoint_prefix = os.path.join(FLAGS.logdir, 'unet_tf2-ckpt')
+    # Setup summary writer.
+    os.makedirs(FLAGS.logdir, exist_ok=True)
+    summary_writer = tf.summary.create_file_writer(
+        os.path.join(FLAGS.logdir, 'train_logs'), flush_millis=10000)
 
-  # Setup checkpoint directory.
-  checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
-  manager = tf.train.CheckpointManager(
-    checkpoint,
-    checkpoint_prefix,
-    max_to_keep=10,
-    keep_checkpoint_every_n_hours=3)
-  # Restores the checkpoint, if existing.
-  checkpoint.restore(manager.latest_checkpoint)
+    # Create a checkpoint directory to store the checkpoints.
+    checkpoint_prefix = os.path.join(FLAGS.logdir, 'unet_tf2-ckpt')
 
-  def train_step(inputs):
-    """Train one batch."""
-    images, labels, masks = inputs
+    # Setup checkpoint directory.
+    checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
+    manager = tf.train.CheckpointManager(
+        checkpoint,
+        checkpoint_prefix,
+        max_to_keep=10,
+        keep_checkpoint_every_n_hours=3)
+    # Restores the checkpoint, if existing.
+    checkpoint.restore(manager.latest_checkpoint)
 
-    with tf.GradientTape() as tape:
-      logits = model.build_call(images, training=True)
-      loss = loss_func(labels, logits)
-    gradients = tape.gradient(loss, model.trainable_weights)
-    clipped, _ = tf.clip_by_global_norm(gradients, clip_norm=tf.constant(10.0))
-    optimizer.apply_gradients(zip(clipped, model.trainable_weights))
+    def train_step(inputs):
+        """Train one batch."""
+        images, labels, masks = inputs
 
-    return loss
+        with tf.GradientTape() as tape:
+            logits = model.build_call(images, training=True)
+            loss = loss_func(labels, logits)
+        gradients = tape.gradient(loss, model.trainable_weights)
+        clipped, _ = tf.clip_by_global_norm(gradients, clip_norm=tf.constant(10.0))
+        optimizer.apply_gradients(zip(clipped, model.trainable_weights))
 
-  global_step_value = optimizer.iterations.numpy()
-  last_summary_step_value = None
-  last_summary_time = None
+        return loss
 
-  while global_step_value < max_iters:
-    input_batch = next(train_iter)
+    global_step_value = optimizer.iterations.numpy()
+    last_summary_step_value = None
+    last_summary_time = None
 
-    # Set learning rate and run the training step over num_gpu gpus.
-    optimizer.learning_rate = _learning_rate_schedule(
-      optimizer.iterations.numpy(), max_iters, initial_lr)
-    loss = train_step(input_batch)
+    while global_step_value < max_iters:
+        input_batch = next(train_iter)
 
-    # Step number, to be used for summary/logging.
-    global_step = optimizer.iterations
-    global_step_value = global_step.numpy()
+        # Set learning rate and run the training step over num_gpu gpus.
+        optimizer.learning_rate = _learning_rate_schedule(
+            optimizer.iterations.numpy(), max_iters, initial_lr)
+        loss = train_step(input_batch)
 
-    # LR, losses and others summaries.
-    with summary_writer.as_default():
-      tf.summary.scalar(
-        'train/learning_rate', optimizer.learning_rate, step=global_step)
-      tf.summary.scalar(
-        'train/loss', loss, step=global_step)
-    
-      # Summary for number of global steps taken per second.
-      current_time = time.time()
-      if (last_summary_step_value is not None and
-          last_summary_time is not None):
-        tf.summary.scalar(
-            'train/global_steps_per_sec',
-            (global_step_value - last_summary_step_value) /
-            (current_time - last_summary_time),
-            step=global_step)
-      last_summary_step_value = global_step_value
-      last_summary_time = current_time
+        # Step number, to be used for summary/logging.
+        global_step = optimizer.iterations
+        global_step_value = global_step.numpy()
 
-    if (global_step_value % FLAGS.save_interval == 0) or (
-        global_step_value >= max_iters):
-      save_path = manager.save(checkpoint_number=global_step_value)
-      
-      weights_path = os.path.join(FLAGS.logdir, 'unet_weights')
-      model.save_weights(weights_path, save_format='tf')
+        # LR, losses and others summaries.
+        with summary_writer.as_default():
+            tf.summary.scalar(
+                'train/learning_rate', optimizer.learning_rate, step=global_step)
+            tf.summary.scalar(
+                'train/loss', loss, step=global_step)
+
+            # Summary for number of global steps taken per second.
+            current_time = time.time()
+            if (last_summary_step_value is not None and
+                    last_summary_time is not None):
+                tf.summary.scalar(
+                    'train/global_steps_per_sec',
+                        (global_step_value - last_summary_step_value) /
+                        (current_time - last_summary_time),
+                        step=global_step)
+            last_summary_step_value = global_step_value
+            last_summary_time = current_time
+
+        if (global_step_value % FLAGS.save_interval == 0) or (
+                global_step_value >= max_iters):
+            save_path = manager.save(checkpoint_number=global_step_value)
+
+            weights_path = os.path.join(FLAGS.logdir, 'unet_weights')
+            os.makedirs(weights_path, exist_ok=True)
+            model.save_weights(weights_path, save_format='tf')
 
 
 if __name__ == '__main__':
-  app.run(main)
+    app.run(main)
